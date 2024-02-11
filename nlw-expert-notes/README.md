c05bb1c460c2b6ba937b3820802d8a476bc9a132
Rodrigo Luigi <digaor@gmail.com>
2024-02-11 18:47:09 -0300

    feat: create home page, NoteCard and NewNoteCard components

--- nlw-expert-notes/README.md
+++ nlw-expert-notes/README.md
@@ -1,3 +1,5 @@
 # Inicializando projeto nlw-expert-notes
 
-- 
+- npm i date-fns (formatação de data)
+- npm i lucide-react (pacote de icons)
+- npm install sonner (lib de toast)
--- nlw-expert-notes/src/app.tsx
+++ nlw-expert-notes/src/app.tsx
@@ -1,5 +1,48 @@
+import { useState } from 'react'
+import logo from './assets/logo-nlw-expert.svg'
+import { NewNoteCard } from './components/NewNoteCard'
+import { NoteCard } from './components/NoteCard'
+
+interface Note {
+  id: string
+  date: Date
+  content: string
+}
+
 export function App() {
+  const [notes, setNotes] = useState<Note[]>([])
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    }
+
+    setNotes([newNote, ...notes])
+  }
+
   return (
-    <h1 className="text-3xl font-bold underline ">Weed World</h1>
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={logo} alt="NLW Expert" />
+
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Busque em suas notas..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none"
+        />
+      </form>
+
+      <div className="h-px bg-slate-700" />
+
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+
+        {notes.map((note) => {
+          return <NoteCard key={note.id} note={note} />
+        })}
+      </div>
+    </div>
   )
 }
